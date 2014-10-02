8c64765e71f86e8b209d2780d945e1461bda7cbe
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-02 17:06:27 +0100

    Added Result, Success and Failure

---
+++ ParsersLib/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace ParsersLib
+{
+    // ReSharper disable UnusedTypeParameter
+    public class Result<TA>
+    {
+    }
+    // ReSharper restore UnusedTypeParameter
+
+    public class Success<TA> : Result<TA>
+    {
+        public TA Value { get; private set; }
+        public int CharsConsumed { get; private set; }
+
+        public Success(TA value, int charsConsumed)
+        {
+            Value = value;
+            CharsConsumed = charsConsumed;
+        }
+    }
+
+    public class Failure<TA> : Result<TA>
+    {
+        public ParseError ParseError { get; private set; }
+
+        public Failure(ParseError parseError)
+        {
+            ParseError = parseError;
+        }
+    }
+}
