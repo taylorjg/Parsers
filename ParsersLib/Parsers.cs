97ad7b701266d7582fe86e1816983fc700e6f0cd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-02 13:10:28 +0100

    Added more primitives and combinators

--- ParsersLib/Parsers.cs
+++ ParsersLib/Parsers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using MonadLib;
 
 namespace ParsersLib
@@ -18,12 +19,11 @@ namespace ParsersLib
 
         // Primitives
         public abstract Parser<string> String(string s);
+        public abstract Parser<string> Regex(Regex r);
         public abstract Parser<string> Slice<TA>(Parser<TA> p);
-        public abstract Parser<TA> Or<TA>(Parser<TA> p1, Func<Parser<TA>> p2Func);
+        public abstract Parser<TA> Succeed<TA>(TA a);
         public abstract Parser<TB> FlatMap<TA, TB>(Parser<TA> p, Func<TA, Parser<TB>> f);
-        public abstract Parser<IEnumerable<TA>> ListOfN<TA>(int n, Parser<TA> p);
-        public abstract Parser<IEnumerable<TA>> Many<TA>(Parser<TA> p);
-        public abstract Parser<IEnumerable<TA>> Many1<TA>(Parser<TA> p);
+        public abstract Parser<TA> Or<TA>(Parser<TA> p1, Func<Parser<TA>> p2Func);
 
         // Combinators
         public Parser<char> Char(char c)
@@ -31,24 +31,43 @@ namespace ParsersLib
             return Map(String(Convert.ToString(c)), a => a.First());
         }
 
-        public Parser<TA> Succeed<TA>(TA a)
-        {
-            return Map(String(string.Empty), _ => a);
-        }
-
         public Parser<TB> Map<TA, TB>(Parser<TA> p, Func<TA, TB> f)
         {
             return FlatMap(p, a => Succeed(f(a)));
         }
 
-        public Parser<TC> Map2<TA, TB, TC>(Parser<TA> p1, Parser<TB> p2, Func<TA, TB, TC> f)
+        public Parser<TC> Map2<TA, TB, TC>(Parser<TA> p1, Func<Parser<TB>> p2, Func<TA, TB, TC> f)
         {
-            return FlatMap(p1, a => Map(p2, b => f(a, b)));
+            return FlatMap(p1, a => Map(p2(), b => f(a, b)));
         }
 
         public Parser<Tuple<TA, TB>> Product<TA, TB>(Parser<TA> p1, Func<Parser<TB>> p2)
         {
             return FlatMap(p1, a => Map(p2(), b => Tuple.Create(a, b)));
+        }
+
+        public Parser<IEnumerable<TA>> Many<TA>(Parser<TA> p)
+        {
+            return Or(
+                Map2(p, () => Many(p), Cons),
+                () => Succeed(Enumerable.Empty<TA>()));
+        }
+
+        public Parser<IEnumerable<TA>> Many1<TA>(Parser<TA> p)
+        {
+            return Map2(p, () => Many(p), Cons);
+        }
+
+        public Parser<IEnumerable<TA>> ListOfN<TA>(int n, Parser<TA> p)
+        {
+            return n <= 0
+                       ? Succeed(Enumerable.Empty<TA>())
+                       : Map2(p, () => ListOfN(n - 1, p), Cons);
+        }
+
+        private static IEnumerable<T> Cons<T>(T x, IEnumerable<T> xs)
+        {
+            return Enumerable.Repeat(x, 1).Concat(xs);
         }
     }
 }
